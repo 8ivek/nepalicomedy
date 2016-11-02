6e5fcbf6793dc0194b4518c377800cc1652c2c1f
Bivek <bivek@evolutionindesignz.com>
2016-11-02 11:00:55 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Hellow</title>
+</head>
+<body>
+    <h1>Title</h1>
+    <div>
+        <p>This is test message.</p>
+    </div>
+    <script src="node_modules/angular/angular.min.js"></script>
+    <script src="node_modules/angular-sanitize/angular-sanitize.min.js"></script>
+    <script src="node_modules/videogular/videogular.js"></script>
+    <script src="node_modules/videogular-controls/vg-controls.js"></script>
+    <script src="node_modules/videogular-overlay-play/vg-overlay-play.js"></script>
+    <script src="node_modules/videogular-poster/vg-poster.js"></script>
+    <script src="node_modules/videogular-buffering/vg-buffering.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
